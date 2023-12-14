0c7aab4a8a37b1e32ebb99131123449072c3ca3d
kjh5848 <kjh5848@gmail.com>
2023-12-14 17:56:43 +0900

    배열

---
+++ src/ex03/ArrayTest4.java
@@ -0,0 +1,13 @@
+package ex03;
+
+import java.util.Arrays;
+
+public class ArrayTest4 {
+    public static void main(String[] args) {
+        int[] number = {10, 20, 30};
+        for (int i = 0; i < number.length; i++) {
+            System.out.printf(Arrays.toString(number));
+
+        }
+    }
+}
---
+++ src/ex03/PizzaTopping.java
@@ -0,0 +1,13 @@
+package ex03;
+
+import java.util.Arrays;
+
+public class PizzaTopping {
+    public static void main(String[] args) {
+        String[] toppings = {"Pepperoni", "Mushrooms", "Oniens", "Sausage", "Bacon"};
+
+        for (String s : toppings) {
+            System.out.printf(s + " ");
+        }
+    }
+}
