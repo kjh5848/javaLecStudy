2581039abc9171ce7ae8e8c2fb4ffd3beba0a32c
kjh5848 <kjh5848@gmail.com>
2023-12-18 14:10:56 +0900

    2진 검색 연습완료

---
+++ src/ex03/insertionSort.java
@@ -0,0 +1,18 @@
+package ex03;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 3, 2, 4};
+
+        int temp;
+        if (arr[0] > arr[1]) {
+            temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+
+//        if (arr[1]> arr[2]) {
+//
+//        }
+    }
+}
