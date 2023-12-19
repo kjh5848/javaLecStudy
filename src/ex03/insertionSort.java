8bf6085831bf8e074cd806f939204df52c1251fd
kjh5848 <kjh5848@gmail.com>
2023-12-19 09:08:33 +0900

    삽입정렬2

--- src/ex03/insertionSort.java
+++ src/ex03/insertionSort.java
@@ -3,23 +3,32 @@ package ex03;
 public class insertionSort {
     public static void main(String[] args) {
         int[] arr = {5, 1, 3, 2, 4};
+        int N = arr.length;
 
         int key,temp,i;
-        key = arr[0];
+
         i = arr[1];
+        for (int j = 0; j < 4; j++) {
+            key = j;
+            if (arr[j] > arr[j+1]) {
+                arr[j+1] = arr[key];
+                key = i;
+            }
 
-        if (arr[key] > arr[key + 1]) {
-            arr[key + 1] = arr[key];
-            key = i;
+        }
+        for (int j = 0; j < N; j++) {
+            System.out.println(arr[i]);
         }
 
-        if (arr[key + 1] > arr[key+2]) {
-            arr[key + 2] = arr[key + 1];
 
-            if ()
-        }
-
-
+//        if (arr[j+1] > arr[j+2]) {
+//            arr[j+2] = arr[j+1];
+//
+//            if (arr[j] > arr[j+1]) {
+//                arr[j+1] = arr[j];
+//                key = i;
+//            }
+//        }
 
     }
 }
