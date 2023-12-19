0cab55e5ed3cb1db549e890d268506cdbacd59d5
kjh5848 <kjh5848@gmail.com>
2023-12-19 18:09:02 +0900

    stackoverflrow

---
+++ src/ex04/StackTest.java
@@ -0,0 +1,13 @@
+package ex04;
+
+public class StackTest {
+
+    static void run() {
+        System.out.println();
+        run();
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
