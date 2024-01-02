30270da144469ea2ec0d43431c7ca07746b4d1ad
kjh5848 <kjh5848@gmail.com>
2024-01-02 12:33:37 +0900

    vim

--- src/ex08/example/StringEx01.java
+++ src/ex08/example/StringEx01.java
@@ -31,11 +31,26 @@ public class StringEx01 {
     public static String decoding(String encodingData) {
         // String data = "A2B3C4D5";
 
+        int countA = 0, countB = 0, countC = 0, countD = 0;
+        String str;
+        for (int i = 0; i < encodingData.length(); i++) {
+            str = encodingData.substring(i,i+1);
+            if (str.equals("A")) {
+                countA++;
+            }
+            if (str.equals("B")) {
+                countB++;
+            }
+            if (str.equals("C")) {
+                countC++;
+            }
+            if (str.equals("D")) {
+                countD++;
+            }
+        }
 
-
-
-
-        return "";
+        System.out.println("A" + countA + "B" + countB + "C" + countC + "D" + countD);
+        return " ";
     }
 
     public static void main(String[] args) {
@@ -47,7 +62,7 @@ public class StringEx01 {
 
 
         // 디코딩 AABBBCCCCDDDDD
-
+        decoding(data);
 
     }
 }
